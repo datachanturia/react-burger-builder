ac8e61c79fa6e1c8e6cf66ff53f0581495742532
dchan13 <davidchanturia13@gmail.com>
2019-03-09 14:22:23 +0400

    com 2

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
-import Auxilary from '../../hoc/Auxilary';
+import Aux from '../../hoc/Auxilary';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
-    <Auxilary>
+    <Aux>
+        <Toolbar />
+        <SideDrawer />
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
         </main>
-    </Auxilary>
+    </Aux>
 );
 
 export default layout;
