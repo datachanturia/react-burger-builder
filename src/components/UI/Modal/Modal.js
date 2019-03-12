68680af6cb9bf74701f993222b0b69876fd5de76
dchan13 <davidchanturia13@gmail.com>
2019-03-12 10:12:53 +0400

    fix ui order summary/button/modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -21,7 +21,7 @@ class Modal extends Component {
                 <div 
                     className={classes.Modal} 
                     style={{
-                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-200vh)',
                         opacitu: this.props.show ? '1': '0'
                     }}>
                     {this.props.children}
